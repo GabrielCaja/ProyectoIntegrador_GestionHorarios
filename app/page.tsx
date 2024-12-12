b816e738eb957995666380418c60ba360945920a
GabrielCaja <gabrielcajaegido@gmail.com>
2024-12-12 11:49:07 +0100

    Refactor LoginPage component for improved code readability and formatting

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function LoginPage() {
       .then(data => {
         if (data.access_token) {
           console.log('Login successful', data);
-        } 
+        }
       })
       .catch(error => {
         console.error('Error:', error);
@@ -43,14 +43,13 @@ export default function LoginPage() {
     <div className="min-h-screen flex items-center justify-center bg-blue-600 to-black p-8">
       <div className="w-full max-w-md ">
         <form onSubmit={handleSubmit} className="bg-white shadow-2xl rounded-lg p-8 space-y-6">
-
-          <Image 
-          src="/logoU-TAD.jpg" 
-          alt="Logo Utad"
-          width={150}
-          height={150}
-          className="object-contain relative mx-auto"
-        />
+          <Image
+            src="/logoU-TAD.jpg"
+            alt="Logo Utad"
+            width={150}
+            height={150}
+            className="object-contain relative mx-auto"
+          />
           <div className="relative">
             <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">
               Email
@@ -118,8 +117,8 @@ export default function LoginPage() {
             />
           </div>
           <div className="transform transition-all duration-300 ease-in-out hover:scale-105">
-            <Button 
-              type="submit" 
+            <Button
+              type="submit"
               className="w-full bg-blue-600 hover:bg-blue-00 text-white font-semibold py-2 px-4 rounded-md transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
             >
               Iniciar Sesión
@@ -135,7 +134,7 @@ export default function LoginPage() {
             </Button>
           </div>
         </form>
-        
+
       </div>
     </div>
   )
